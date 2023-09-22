e71e5d26a538930eadc554c18e3ae0c518c49881
Max Long <mel11691@gmail.com>
2023-09-22 12:47:44 -0500

    First Commit

---
+++ shapes/square.js
@@ -0,0 +1,11 @@
+// Define the square shape (level 1)
+export const squareShape = {
+    isPointInShape: (x, y) => {
+        // Define square boundaries (adjust as needed)
+        const minX = 10;
+        const maxX = 20;
+        const minY = 10;
+        const maxY = 20;
+        return x >= minX && x <= maxX && y >= minY && y <= maxY;
+    }
+};
