f1d6d1f7f83dd6e0cff5d3737fc5b6532705e40e
Max Long <mel11691@gmail.com>
2023-09-25 18:27:29 -0500

    aider: Moved relevant parts of code to `popup.js` and added event listeners for mousemove and mouseout.
    # Aider chat conversation:
    
    ASSISTANT: Alright, let's start with moving the relevant parts of the code to the `popup.js` module.
    
    ```javascript
    main.js
    <<<<<<< HEAD
    // Add event listeners for mousemove
    canvas.addEventListener("mousemove", (e) => {
        const x = Math.floor(e.offsetX / cellSize);
        const y = Math.floor(e.offsetY / cellSize);
        const yangValue = terrain[x][y].yang;
        const yinValue = terrain[x][y].yin;
        popup.innerHTML = `Yang: ${yangValue}<br>Yin: ${yinValue}`;
        popup.style.left = `${e.clientX + 10}px`;
        popup.style.top = `${e.clientY}px`;
        popup.style.display = "block";
    });
    
    // Add mouseout event listener to hide the tooltip
    canvas.addEventListener("mouseout", () => {
        popup.style.display = "none";
    });
    =======
    // Code moved to popup.js
    >>>>>>> updated
    ```
    
    ```javascript
    popup.js
    <<<<<<< HEAD
    =======
    // Add event listeners for mousemove
    canvas.addEventListener("mousemove", (e) => {
        const x = Math.floor(e.offsetX / cellSize);
        const y = Math.floor(e.offsetY / cellSize);
        const yangValue = terrain[x][y].yang;
        const yinValue = terrain[x][y].yin;
        popup.innerHTML = `Yang: ${yangValue}<br>Yin: ${yinValue}`;
        popup.style.left = `${e.clientX + 10}px`;
        popup.style.top = `${e.clientY}px`;
        popup.style.display = "block";
    });
    
    // Add mouseout event listener to hide the tooltip
    canvas.addEventListener("mouseout", () => {
        popup.style.display = "none";
    });
    >>>>>>> updated
    ```
    
    Next, we'll move the relevant parts of the code to the `slider.js` module. This will be a larger change, so I'll break it down into multiple edit blocks.

--- main.js
+++ main.js
@@ -268,22 +268,7 @@ if (areSlidersExclusivelyWithinShape()) {
 }
 }
 
-// Add event listeners for mousemove
-canvas.addEventListener("mousemove", (e) => {
-    const x = Math.floor(e.offsetX / cellSize);
-    const y = Math.floor(e.offsetY / cellSize);
-    const yangValue = terrain[x][y].yang;
-    const yinValue = terrain[x][y].yin;
-    popup.innerHTML = `Yang: ${yangValue}<br>Yin: ${yinValue}`;
-    popup.style.left = `${e.clientX + 10}px`;
-    popup.style.top = `${e.clientY}px`;
-    popup.style.display = "block";
-});
-
-// Add mouseout event listener to hide the tooltip
-canvas.addEventListener("mouseout", () => {
-    popup.style.display = "none";
-});
+// Code moved to popup.js
 
 // Initial terrain draw
 drawTerrain();
--- popup.js
+++ popup.js
@@ -14,3 +14,19 @@ canvas.addEventListener("mousemove", (e) => {
 canvas.addEventListener("mouseout", () => {
     popup.style.display = "none";
 });
+// Add event listeners for mousemove
+canvas.addEventListener("mousemove", (e) => {
+    const x = Math.floor(e.offsetX / cellSize);
+    const y = Math.floor(e.offsetY / cellSize);
+    const yangValue = terrain[x][y].yang;
+    const yinValue = terrain[x][y].yin;
+    popup.innerHTML = `Yang: ${yangValue}<br>Yin: ${yinValue}`;
+    popup.style.left = `${e.clientX + 10}px`;
+    popup.style.top = `${e.clientY}px`;
+    popup.style.display = "block";
+});
+
+// Add mouseout event listener to hide the tooltip
+canvas.addEventListener("mouseout", () => {
+    popup.style.display = "none";
+});
