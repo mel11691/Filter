3856ce4c75fd4cd9866744989977e005ef945406
Max Long <mel11691@gmail.com>
2023-10-29 16:38:38 -0500

    star approximation

--- shapes/star.js
+++ shapes/star.js
@@ -1,39 +1,28 @@
-// Define the star shape hi (level 3)
+// Define the star shape
 const width = 30;
 const height = 30;
 
 // Scaling factor; larger values make the star smaller, smaller values make it larger
-const scaleFactor = 0.10;
+const scaleFactor = 0.1;
+
+// Thickness factor; larger values make the arms of the star thicker
+const thicknessFactor = 0.7;
 
 export const starShape = {
     name: "star",
     isPointInShape: (x, y) => {
-        // Define the vertices of the star
-        const vertices = [
-            [width / 2, 0],
-            [width, height],
-            [0, height / 2],
-            [width, height / 2],
-            [0, height]
-        ];
+        // Center of the star
+        const centerX = width / 2;
+        const centerY = height / 2;
 
-        // Calculate the area of the star
-        const starArea = calculateArea(vertices[0], vertices[1], vertices[2]) +
-                         calculateArea(vertices[0], vertices[2], vertices[3]) +
-                         calculateArea(vertices[0], vertices[3], vertices[4]);
+        // Transform coordinates to make (centerX, centerY) the origin and apply scaling
+        const gridX = (x - centerX) * scaleFactor;
+        const gridY = (y - centerY) * scaleFactor;
 
-        // Calculate the sum of the areas of the triangles formed by the point and each pair of vertices
-        const pointArea = calculateArea(vertices[0], vertices[1], [x, y]) +
-                          calculateArea(vertices[1], vertices[2], [x, y]) +
-                          calculateArea(vertices[2], vertices[3], [x, y]) +
-                          calculateArea(vertices[3], vertices[4], [x, y]) +
-                          calculateArea(vertices[4], vertices[0], [x, y]);
-
-        // The point is inside the star if the sum of the areas is equal to the area of the star
-        return Math.abs(starArea - pointArea) < 0.01;
+        // Use the equation for a 5-pointed star to determine if the point is inside the star
+        const r = Math.sqrt(Math.pow(gridX, 2) + Math.pow(gridY, 2));
+        const theta = Math.atan2(gridY, gridX);
+        const starEdge = 0.5 * (1 - Math.sin(5 * theta)) + thicknessFactor;
+        return r <= starEdge;
     }
 };
-    // Helper function to calculate the area of a triangle given its vertices
-    calculateArea: (v1, v2, v3) => {
-        return 0.5 * Math.abs(v1[0]*(v2[1]-v3[1]) + v2[0]*(v3[1]-v1[1]) + v3[0]*(v1[1]-v2[1]));
-    }
