6899d166c1a58ec240d48409242bfb35b6d2586a
Max Long <mel11691@gmail.com>
2023-10-30 13:42:39 -0500

    made square larger

--- shapes/square.js
+++ shapes/square.js
@@ -3,10 +3,10 @@ export const squareShape = {
     name: 'square',
     isPointInShape: (x, y) => {
         // Define square boundaries (adjust as needed)
-        const minX = 10;
-        const maxX = 20;
-        const minY = 10;
-        const maxY = 20;
+        const minX = 7.5;
+        const maxX = 22.5;
+        const minY = 7.5;
+        const maxY = 22.5;
         return x >= minX && x <= maxX && y >= minY && y <= maxY;
     }
 };
